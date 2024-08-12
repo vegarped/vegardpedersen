aa4109a3123a1e91cd7a705afc05cd75215b16ec
Vegard Pedersen <vegardpedersen@Vegards-MacBook-Air.local>
2024-08-12 18:03:13 +0200

    Aliases

---
+++ aliases.zsh
@@ -0,0 +1,35 @@
+# ====ALIAS-CONFIGURATION====
+
+# Go back
+  alias ..="cd .."
+
+# Go to vegardpedersen
+  alias vp="cd ~/vegardpedersen"
+
+# Open vscode
+  alias c="code ."
+
+# Git add
+  alias ga="git add"
+
+# Git commit
+  alias gc="git commit"
+
+# Git status
+  alias gs="git status"
+
+# Git push
+  alias gp="git pull"
+
+# Git checkout main
+  alias gcm="git checkout main"
+
+# Git checkout new branch
+  alias gcn="git checkout -b"
+
+# Git branch
+  alias gb="git branch"
+
+# Show previous branches
+  function branch() {git checkout $(git for-each-ref --sort=-committerdate refs/heads/ --format='%(refname:short)|%(committerdate:relative)|%(authorname)' --color=always|column -ts'|' | fzf | cut -f 1 -d ' ')}
+  alias b='branch'
