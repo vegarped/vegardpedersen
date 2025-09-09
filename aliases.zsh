523a31c6dbf776f4e2bd0a8706e9fe89f0aaa177
Vegard Pedersen <44409957+vegarped@users.noreply.github.com>
2025-09-09 11:23:45 +0200

    Add openpr to aliases

--- aliases.zsh
+++ aliases.zsh
@@ -34,6 +34,14 @@
   function branch() {git checkout $(git for-each-ref --sort=-committerdate refs/heads/ --format='%(refname:short)|%(committerdate:relative)|%(authorname)' --color=always|column -ts'|' | fzf | cut -f 1 -d ' ')}
   alias b='branch'
 
+# Open the Pull Request URL for your current directory's branch (base branch defaults to main)
+  function openpr() {
+    github_url=`git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#https://#' -e 's@com:@com/@' -e 's%\.git$%%' | awk '/github/'`;
+    branch_name=`git symbolic-ref HEAD | cut -d"/" -f 3,4`;
+    pr_url=$github_url"/compare/main..."$branch_name
+    open $pr_url;
+  }
+
 # Check, build and run Cargo
   alias cc="cargo check"
   alias cb="cargo build"
